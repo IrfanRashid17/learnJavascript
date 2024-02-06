2ad1ec736ca35585351fc812ca85804221369d96
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-07 00:03:49 +0530

    arrays in javascript

---
+++ 12_Arrays.js
@@ -0,0 +1,12 @@
+const array = [0,1,2,3,4,5,6,7,8,9,'irfan rashid']   //data items can be different 
+console.log(array)
+console.log(array[10])   //location wise
+
+//SHALLOW COPY : A shallow copy of an object is a copy whose properties share the same references(point to the same underlying values)
+
+//DEEP COPY : A deep copy of an object is a copy whose properties do not share the same references 
+
+//********* Another method of declaring an array 
+const myArr =new Array(1,2,3,4,"irfan rashid")
+console.log(myArr)
+
