6a9f90a9074d9b49030f1565ffb61b740c6457c9
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-09 00:13:43 +0530

    objects part one (literals, [ ] notation, Symbol, freeze , greeting, and this

--- 14_objects.js
+++ 14_objects.js
@@ -31,12 +31,23 @@ console.log(fruits[mySymbol])
 
 //*************** other functions in objects */
 jsUser.email = "irfan@amazon.com"        // updated email id here
-Object.freeze(jsUser)                   //freezed email id here
+//Object.freeze(jsUser)                   //freezed email id here
 jsUser.email = "irfan@microsoft.com"    // attempting to modify but cannot perform email id is freezed 
 console.log(jsUser["email"])
 
 //adding function in objects
 jsUser.greeting = function(){
-    console.log("Hello Code reader")
+    console.log("Hello programmer")
 }
-console.log(jsUser.greeting())
+
+console.log(jsUser.greeting)        //output: [Function (anonymous)]
+console.log(jsUser.greeting())      //Hello Code reader  //undefined
+
+//_______________ String Interpolation ________________
+jsUser.greetingTwo = function(){
+    console.log(`Hello Programmer, ${this.name}`)   //********* String Interpolation */
+}
+
+console.log(jsUser.greetingTwo())      //Hello Code reader, Irfan Rashid   //undefined
+
+                                    //Ends
