a922ff0c835ccbfad84d002e1e741e4f05ca8ae9
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-12 00:59:34 +0530

    added keys( ), values( ), entries( ), hasOwnProperty( )

--- 15_objects2.js
+++ 15_objects2.js
@@ -33,3 +33,39 @@ console.log(obj4)
 
 const obj5 = {...obj1,...obj2}    //SPREAD AND COMBINE 
 console.log(obj5)
+
+//when values come from databases
+const users = [                  //array can have multiple objects
+    {                           //ist object
+    name: "iriz",
+    id:123,
+    email: "iriz@gmail.com"
+    },
+
+    {                           //2nd object
+        name: "user2",
+        id: 456,
+        ph : 9906012345, 
+        address: "abc"
+    },
+]
+
+console.log(users)
+/*Output:    [
+  { name: 'iriz', id: 123, email: 'iriz@gmail.com' },
+  { name: 'user2', id: 456, ph: 9906012345, address: 'abc' }
+]       */
+console.log(users[0].email)    //prints targetted value only
+
+//********************************** 
+console.log(guestUser)          //defined at top as // { id: '123abc', name: 'Irfan', isLoggedIn: false }
+console.log(Object.keys(guestUser))     // prints [ 'id', 'name', 'isLoggedIn' ] i.e with datatype array
+//Note : it is used too much
+
+console.log(Object.values(guestUser))       //SHOWS THE VALUES AS AN ARRAY  
+//[ '123abc', 'Irfan', false ]
+
+console.log(Object.entries(guestUser))      //SHOWS ALL THE ENTRIES WITH KEYS AS ARRAYS
+//[ [ 'id', '123abc' ], [ 'name', 'Irfan' ], [ 'isLoggedIn', false ] ]
+
+console.log(guestUser.hasOwnProperty('id'))     //whether it contains a particular here 'id'
