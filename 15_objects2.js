d432965357d1efa416aacf18fa1758eb698b470d
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-12 00:00:17 +0530

    added

--- 15_objects2.js
+++ 15_objects2.js
@@ -17,9 +17,19 @@ const owner = {
                }
 }
 
-console.log(owner.fullname.userFullName.firstName)
+console.log(owner.fullname.userFullName.lastName)
 
-console.log(owner.fullname?.userFullName.firstName) //? adds some protection like if the variable or object doesn't exist
+console.log(owner.fullname?.userFullName.firstName) // Note: '?'  adds some protection like if the variable or object doesn't exist
 //we can avoid if else etc by this
 
+const obj1 = { 1: "a", 2:"b", 3:"c" }
+const obj2 = { 4:"d", 5:"e"}
 
+const obj3 = {obj1,obj2}
+console.log(obj3)
+
+const obj4 = Object.assign({},obj1,obj2)         //this method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object   //it merges
+console.log(obj4)
+
+const obj5 = {...obj1,...obj2}    //SPREAD AND COMBINE 
+console.log(obj5)
