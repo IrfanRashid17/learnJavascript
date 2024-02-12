0a818e35ffd28996b47c1670daf8cf916cd0bd82
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-12 23:33:42 +0530

    JSON IN JS with short notes

--- 16_objects3And_JSON.js
+++ 16_objects3And_JSON.js
@@ -18,5 +18,50 @@ console.log(instructor)
 //({ anything in this assume it is de-structuring})
 
 
+//****************************** JSON IN JS ***************************** */
+//Remember: JSON names require double quotes. JavaScript names do not.
+/* JSON stands for JavaScript Object Notation 
+It is a lightweight data interchange format.
+It is language independent
+It is self-describing 
+JSON Example
+This JSON syntax defines an employees object: an array of 3 employee records (objects):
 
+*********** JSON Example **********/
+
+{
+"employees": [
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"Anna", "lastName":"Smith"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+/*JSON Data - A Name and a Value
+JSON data is written as name/value pairs, just like JavaScript object properties.
+
+A name/value pair consists of a field name (in double quotes), followed by a colon, followed by a value:
+
+"firstName":"John"
+JSON names require double quotes. JavaScript names do not.*/
+
+/************************** JSON Arrays ************************************
+JSON arrays are written inside square brackets.
+
+Just like in JavaScript, an array can contain objects:
+*/
+
+{
+"employees": [
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"Anna", "lastName":"Smith"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+//  *********************** useful api ******************* 
+// random user me api
+
+//******************** tool for understanding JSON in better way **************
+//jsonformatter.org
 
