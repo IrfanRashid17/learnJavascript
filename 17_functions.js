81e6571236bc08bdc6fca54915eb0d47e932aa8f
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-16 23:57:26 +0530

    basic of functions

---
+++ 17_functions.js
@@ -0,0 +1,15 @@
+function myName() {
+    console.log("I")
+    console.log("R")
+    console.log("F")
+    console.log("A")
+    console.log("N")
+}
+
+myName()  //execution
+
+function sum(num1,num2){
+    const sum = num1 + num2
+    console.log(sum)
+}
+sum(3,7)
