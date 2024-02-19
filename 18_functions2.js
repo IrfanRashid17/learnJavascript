38b792e38661f6a833510db9ac453485734231d7
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-19 23:44:11 +0530

    passing objects

--- 18_functions2.js
+++ 18_functions2.js
@@ -9,3 +9,17 @@ function calculateCartPrice2(...num1){     //... three dots are callled as rest 
 }
 console.log(calculateCartPrice2(200,300,400))    //will print all values as an array
 
+//********* Passing objects***************/
+const user = {
+    username: "Irfan",
+    price: 999
+}
+
+function handleObjects(anyobject){
+    console.log(`Username is ${anyobject.username} and the price is ${anyobject.price}`)
+}
+
+handleObjects(user)
+//handleObjects({username: "Sam", price: 777})   //passing values during execution
+
+
