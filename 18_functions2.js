1fd590ff27ca1937b0c7b9997fe837fe8bde6e18
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-19 23:54:29 +0530

    passing Arrays in functions

--- 18_functions2.js
+++ 18_functions2.js
@@ -9,7 +9,7 @@ function calculateCartPrice2(...num1){     //... three dots are callled as rest 
 }
 console.log(calculateCartPrice2(200,300,400))    //will print all values as an array
 
-//********* Passing objects***************/
+                        //********* Passing objects***************/
 const user = {
     username: "Irfan",
     price: 999
@@ -23,3 +23,10 @@ handleObjects(user)
 //handleObjects({username: "Sam", price: 777})   //passing values during execution
 
 
+            //************************** Passing Arrays ***************************** */
+const newArray = [11,22,33,44,55]
+function arrayFunction(getArray){
+    return getArray[1]
+}
+console.log(arrayFunction(newArray))
+console.log(arrayFunction([10,20,30,40,50]))      //passing different values other than the already specified
