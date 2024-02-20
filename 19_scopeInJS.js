ef0eae650fb354b2089f40f92ce9d24c452cbfb8
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-20 23:57:27 +0530

    var scope

---
+++ 19_scopeInJS.js
@@ -0,0 +1,9 @@
+var trying = 999
+ 
+if (true){
+    let a = 10
+    const b = 20
+    var trying = 30
+    console.log("Inner IF :", trying) 
+}
+console.log(trying)          //it will overide the variable TRYING= 999 with 30, which was global but now changed by local block
