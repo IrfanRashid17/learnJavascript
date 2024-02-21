25c7143b9757836d7d1ad26fcc11b617dd8a31a7
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-21 10:35:40 +0530

    scope in js and  for loop

--- 19_scopeInJS.js
+++ 19_scopeInJS.js
@@ -6,4 +6,8 @@ if (true){
     var trying = 30
     console.log("Inner IF :", trying) 
 }
-console.log(trying)          //it will overide the variable TRYING= 999 with 30, which was global but now changed by local block
+console.log(trying)   //it will overide the variable TRYING= 999 with 30, which was global but now changed by local block
+
+for(i=0; i<ArrayBuffer.length;i++){     //'i' is used inside the block
+    const element= array[i]
+}
