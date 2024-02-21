5f038090ac19ad02a1e71971d4280e09170d038c
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-21 12:25:58 +0530

    scope in js completed with some important points

--- 19_scopeInJS.js
+++ 19_scopeInJS.js
@@ -12,7 +12,7 @@ console.log(trying)   //it will overide the variable TRYING= 999 with 30, which 
     const element= array[i]
 }*/
 
-//++++++++++++++++ function inside function (Scope within block and outside the block) ++++++++++++++++++
+        //++++++++++++++++ function inside function (Scope within block and outside the block) ++++++++++++++++++
 function outerFunction(){
     let username = "Irfan"
     function innerFunction(){
@@ -23,11 +23,29 @@ function outerFunction(){
 }
 outerFunction()
 
-// ******************* Example 2: *********************
+                // ******************* Example 2: *********************
 if(true){
     const username = "If Irfan"
     if(username === "If Irfan"){
         const father = " Rashid"
         console.log(username + father)
     }
+    //console.log(father)    //it will return error as website is inaccessible
 }
+//console.log(username)     //error 
+
+
+// ********************** IMPORTANT **********************
+console.log(adding(8))     // can be also called like this
+function adding(num){
+    return num + 6          //output is: 8+6 = 14
+}
+//console.log(adding(8))
+//adding(8)
+
+
+//console.log(adding2(8))   //  ERRROR : As a function cannot be called here by using this type of function declarationI.e using variable as function holder
+const adding2 = function(num){   //Another way of declaring a function 
+    return num + 3  
+}
+console.log(adding2(8))      //By declaring a function like this method we can call a function only after a funtion declaring
