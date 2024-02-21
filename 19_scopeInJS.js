70781d462843b21b9ffe5b5213446543639826d2
Irfan Rashid <salfiirfan7866@gmail.com>
2024-02-21 11:57:00 +0530

    block scope (E,g in function and if statement etc)

--- 19_scopeInJS.js
+++ 19_scopeInJS.js
@@ -8,6 +8,26 @@ if (true){
 }
 console.log(trying)   //it will overide the variable TRYING= 999 with 30, which was global but now changed by local block
 
-for(i=0; i<ArrayBuffer.length;i++){     //'i' is used inside the block
+/*for(i=0; i<ArrayBuffer.length;i++){     //'i' is used inside the block
     const element= array[i]
+}*/
+
+//++++++++++++++++ function inside function (Scope within block and outside the block) ++++++++++++++++++
+function outerFunction(){
+    let username = "Irfan"
+    function innerFunction(){
+        const innerUser = username     
+        console.log(username)       //innerFunction can access the outerFuctions variable but outerFunction cannot access the innerFunction variables
+    }
+    innerFunction()
+}
+outerFunction()
+
+// ******************* Example 2: *********************
+if(true){
+    const username = "If Irfan"
+    if(username === "If Irfan"){
+        const father = " Rashid"
+        console.log(username + father)
+    }
 }
